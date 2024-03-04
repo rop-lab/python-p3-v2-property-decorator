dc97f361d0145161372359c3f12189942d29bb0e
cheruiyotrop <cheruiyot.rop@student.moringaschool.com>
2024-03-04 07:09:32 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -9,7 +9,6 @@ APPROVED_BREEDS = [
     "Pointer"
 ]
 
-
 class Dog:
     def __init__(self, name='Fido', breed='Mastiff'):
         self.name = name
@@ -19,28 +18,24 @@ class Dog:
     def name(self):
         """The name property"""
         return self._name
-    
+
     @name.setter
     def name(self, name):
-        """Name must be a string between 1 and 25 characters in length"""
         if isinstance(name, str) and 1 <= len(name) <= 25:
             self._name = name.title()
         else:
             raise ValueError(
-                "Name must be string between 1 and 25 characters."
-            )
-    
+                "Name must be a string between 1 and 25 characters.")
+
     @property
     def breed(self):
         """The breed property"""
         return self._breed
-     
+    
     @breed.setter
-    def set_breed(self, breed):
-        """Breed must be in the list of aprooved breeds"""
+    def breed(self, breed):
         if breed in APPROVED_BREEDS:
             self._breed = breed
         else:
-            raise ValueError("Breed must be in list of approved breeds.")
+            raise ValueError("Breed must be in the list of approved breeds.")
 
-    
